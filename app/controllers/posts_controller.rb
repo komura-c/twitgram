6c3189e56eb52da2e329264141ed2288663c4a2d
komura <affirman03139@outlook.jp>
2020-03-05 20:19:44 +0900

    update to see index not login

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
 
   before_action :set_post, only: %i[show destroy]
 
