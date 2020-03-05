239dbaa83e3d8c9949c6c2d9f63460edcaeea93a
komura <affirman03139@outlook.jp>
2020-03-05 23:41:50 +0900

    fix s3 fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
-unless Rails.env.development? || Rails.env.test?
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
+    config.fog_directory = ENV['AWS_S3_BUCKET']
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
+      region: ENV['AWS_S3_REGION'],
+      aws_access_key_id: ENV['AWS_S3_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY']
     }
-    config.fog_directory = 'rails-photo-0'
-    config.storage = :fog
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
 end
